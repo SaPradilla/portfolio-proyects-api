a7621dceeae9a7e627155925d348a133d34d8853
SaPradi <a.garces@globant.com>
2024-09-24 22:10:21 -0500

    fix:  useNewUrlParser is a deprecated option:

--- src/db_connection.js
+++ src/db_connection.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const conectionDB = () =>{
 
-    mongoose.connect(process.env.URI_MONGO,
-        { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    mongoose.connect(process.env.URI_MONGO)
     const db = mongoose.connection
     
     db.on("error",console.error.bind(console,"conection failed: "))

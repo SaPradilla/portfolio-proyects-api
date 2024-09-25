e73490b0351fd7c1ad28ec13042f4b4e2722aca2
SaPradi <a.garces@globant.com>
2024-09-24 21:28:14 -0500

    fix: change app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from 'cors';
-import Routes from './src/routes/index.js';
-import { conectionDB } from "./src/db_connection.js";
+import Routes from './routes/index.js';
+import { conectionDB } from "./db_connection.js";
 
 const app = express();
 
